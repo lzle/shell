7318f9eebdb8124f9a5df4b50a7b9282c1748487
连志雷 <lianzhilei0711@163.com>
2022-05-16 19:07:00 +0800

    Update remove_cluster_nodes.sh

--- example/remove_cluster_nodes.sh
+++ example/remove_cluster_nodes.sh
@@ -6,3 +6,11 @@ cat remove_ips.txt| while read line; do echo $line; sed -i ''  "/$line:/d" baish
 
 cat remove_ips.txt| while read line; do echo $line; grep "$line$" baishan-2copy/hosts.yaml ; done
 cat remove_ips.txt| while read line; do echo $line; sed -i ''  "/$line$/d" baishan-2copy/hosts.yaml ; done
+
+
+
+# s2mgr
+cat /tmp/remove_ips.txt | while read line; do s2mgr.py -e 'partition remove $line'; done
+cat /tmp/remove_ips.txt | while read line; do s2mgr.py -e "node role unset Storage  $line"; done
+cat /tmp/remove_ips.txt | while read line; do s2mgr.py -e "node role unset Memcached  $line"; done
+cat /tmp/remove_nodes.txt | while read line; do s2mgr.py -e "node delete $line"; done

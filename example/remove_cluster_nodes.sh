a69243558de386b37dcc104928470222c1059b88
连志雷 <lianzhilei0711@163.com>
2022-05-16 18:33:10 +0800

    Update remove_cluster_nodes.sh

--- example/remove_cluster_nodes.sh
+++ example/remove_cluster_nodes.sh
@@ -1,7 +1,7 @@
 cat remove_ips.txt| while read line; do echo $line; grep "$line " all-node.md; done
 cat remove_ips.txt| while read line; do echo $line; sed -i '' "/$line /d" all-node.md ;done
 
-cat remove_ips.txt| while read line; do grep "$line" baishan-2copy/indexed_hosts.yaml ; done
+cat remove_ips.txt| while read line; do grep "$line:" baishan-2copy/indexed_hosts.yaml ; done
 cat remove_ips.txt| while read line; do echo $line; sed -i ''  "/$line:/d" baishan-2copy/indexed_hosts.yaml ; done
 
 cat remove_ips.txt| while read line; do echo $line; grep "$line$" baishan-2copy/hosts.yaml ; done
